273d4081bccfd6106710e7474118f2851c554736
Tiago Dias <tigo@webdeleve.net>
2022-02-26 22:56:54 -0300

    PROG SIS I Aula 2 - Arrays - Metodos

---
+++ src/main/java/tiagodias/N_PROG_SIS_I_Aula2_Arrays.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tiagodias;
+
+/**
+ *
+ * @author tiago
+ */
+public class N_PROG_SIS_I_Aula2_Arrays {
+   
+    
+    public static void main(String[] args) {
+    
+        int[] vet = {23, 45, 67, 10};
+        
+        // mapeia pelo elemento
+        for (int x:vet) {
+            System.out.println(x);
+        }
+        
+        
+        // mapeia pelo índice
+        for(int i=0; i<vet.length; i++) {
+            System.out.println(vet[i]);
+        }
+        
+        
+        
+    
+    }
+    
+    
+    
+    
+}
