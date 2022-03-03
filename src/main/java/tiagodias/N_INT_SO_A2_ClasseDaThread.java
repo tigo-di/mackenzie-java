9c0ba03d4d65e3258c0aedc4e76a3fc41af200b2
Tiago Dias <tigo@webdeleve.net>
2022-03-03 02:02:05 -0300

    N_INT_SO_A2 - Thread

---
+++ src/main/java/tiagodias/N_INT_SO_A2_ClasseDaThread.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tiagodias;
+
+/**
+ *
+ * @author tiago
+ */
+public class N_INT_SO_A2_ClasseDaThread extends Thread {
+    
+    public void run() {
+
+        
+        for(int i=0; i<100; i++) {
+        
+            System.out.printf("Classe da Thread - Oi número %d\n", i+1);
+
+        }
+        
+    }
+    
+}
